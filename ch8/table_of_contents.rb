ed836aa6c4a7d02a7c0dfd33706be12a60b056e2
kenhinh1 <40265462+kenhinh1@users.noreply.github.com>
2018-06-23 17:14:03 -0700

    ch08/table_of_contents.rb

---
+++ ch8/table_of_contents.rb
@@ -0,0 +1,14 @@
+toc = ['Table of Contents', 'Chapter 1', 'The Boy Who Lived',
+        'page 1', 'Chapter 2', 'The Vanishing Glass', 'page 13',
+        'Chapter 3', 'The Letters from No One', 'page 21']
+
+        line_width = 30
+
+        puts(              toc[0].center(line_width*3))
+        puts
+        puts(              toc[1].ljust(line_width) +
+toc[2].ljust(line_width) + toc[3].ljust(line_width))
+        puts(              toc[4].ljust(line_width) +
+toc[5].ljust(line_width) + toc[6].ljust(line_width))
+        puts(              toc[7].ljust(line_width) +
+toc[8].ljust(line_width) + toc[9].ljust(line_width))
